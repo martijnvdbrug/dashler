19e75f0889ef01657d062e7c5e9162b8dfd2bce5
martijn <martijn.brug@gmail.com>
2019-09-16 20:40:31 +0200

    Working on dougnut chart for uptime

--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import Chart from 'chart.js'
 
 @Component({
   selector: 'app-block',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockComponent implements OnInit {
 
-  constructor() { }
+  chartId = String(Math.floor(Math.random()* 100000));
 
   ngOnInit() {
+    const ctx = (document.getElementById('chart-uptime')as HTMLCanvasElement).getContext('2d');
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: [2478,5267,734,784,433]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+    });
   }
 
 }
