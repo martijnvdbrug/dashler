55427fb2fd966ff8e18f79367b3b495e30244f5f
martijn <martijn.brug@gmail.com>
2019-09-17 19:01:47 +0200

    Added typescript models for the blocks

--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -1,5 +1,6 @@
-import {AfterViewChecked, AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import Chart from 'chart.js'
+import {uuid} from '../../../lib/uuid';
 
 @Component({
   selector: 'app-block',
@@ -8,29 +9,30 @@ import Chart from 'chart.js'
 })
 export class BlockComponent implements AfterViewInit {
 
+
   chartId;
+  chart: Chart;
 
   constructor() {
-    this.chartId = String(Math.floor(Math.random()* 100000000));
+    this.chartId = uuid();
   }
 
   ngAfterViewInit() {
     console.log('aftervieww', this.chartId);
-    const ctx = (document.getElementById(`chart-${this.chartId}`)as HTMLCanvasElement).getContext('2d');
-    new Chart(ctx, {
+    const ctx = (document.getElementById(`chart-${this.chartId}`) as HTMLCanvasElement).getContext('2d');
+    this.chart = new Chart(ctx, {
       type: 'doughnut',
       data: {
-        // labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        // labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
         datasets: [
           {
-            label: "Population (millions)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-            data: [2478,5267,734,784,433]
+            label: 'Population (millions)',
+            backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+            data: [2478, 5267, 734, 784, 433]
           }
         ]
       },
-      options: {
-      }
+      options: {}
     });
   }
 
