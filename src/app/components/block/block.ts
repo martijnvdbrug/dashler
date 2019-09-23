c009badfb9b731297df745c3b67ee4dbf941a78a
martijn <martijn.brug@gmail.com>
2019-09-23 09:28:08 +0200

    moved into front-end folder

--- src/app/components/block/block.ts
+++
@@ -1,9 +0,0 @@
-import {ResponseTime} from './response-time';
-import {Button} from './button';
-
-export interface Block {
-  id: string;
-  name: string;
-  responseTime: ResponseTime;
-  buttons: Button[];
-}
