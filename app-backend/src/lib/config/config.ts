ae8ce9666695dce257c6b396ad2577f279f782c8
martijn <martijn.brug@gmail.com>
2019-12-19 17:59:51 +0100

    added test plan comments + landingpage small change

--- app-backend/src/lib/config/config.ts
+++ app-backend/src/lib/config/config.ts
@@ -2,10 +2,11 @@ import {ConfigInterface} from './config.interface';
 
 require('dotenv').config();
 
+
 export const CONFIG = process.env as unknown as ConfigInterface;
 if (process.env.NODE_ENV !== 'production') {
   CONFIG.host = 'http://localhost:8999';
   CONFIG.appHost = 'http://localhost:4200';
-  CONFIG.stripePROPlan = 'plan_GHmsOo2Zs0yjdQ';
-  CONFIG.stripeWebhookSecret = 'whsec_GHBVnpmO9UsKkusjHSJhHUTaX24zuHF2';
+  CONFIG.stripePROPlan = 'plan_GHmsOo2Zs0yjdQ'; // test plan
+  CONFIG.stripeWebhookSecret = 'whsec_GHBVnpmO9UsKkusjHSJhHUTaX24zuHF2'; // test secret
 }
